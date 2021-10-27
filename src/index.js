8a973017a6f39e426bf5ae58ff3287538a1ac54d
Abdihakiim <cabdixakiin121@gmail.com>
2021-10-27 16:46:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ require('dotenv').config();
 var status = require('http-status')
 const express = require('express');
 const app = express();
-//const morgan = require('morgan')
+const morgan = require('morgan')
 const interface = require('./router/interface');
 const logger = require('./config/logger');
 const morganMiddleware = require('./middleware/morgan');
-//const bodyParser = require('body-parser');
-let { ApiError } = require ('./payload/apErrors');
+let { ApiError } = require('./payload/apErrors');
 let cors = require('cors');
 let helmet = require('helmet');
 const httpStatus = require('http-status');
@@ -16,31 +15,26 @@ const { error } = require('winston');
 
 const port = process.env.PORT;
 app.use(express.json());
-//app.use(morgan(':url :method :status'));
 app.use(morganMiddleware);
 app.use(process.env.API_VERSION, interface);
 // app.use(helmet());
- app.use(cors());
+app.use(cors());
 logger.warn("Hello From Logger");
 
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
 
     let status = httpStatus.NOT_FOUND;
     //let message = "Not Found"
     let error = 'API Not Found'
 
-   res.status(status).send( new ApiError(status, Error ));
+    res.status(status).send(new ApiError(status, Error));
 
 });
 //Error Handling Exception
-app.use((err, req,res,next) =>{
-
-//    console.error(err.stack);
-
-   res.status(401).send(err);
-
+app.use((err, req, res, next) => {
+    res.status(401).send(err);
 });
-app.listen(port, (req, res)=>{
-    console.log("We are listening Port:"+port);
+app.listen(port, (req, res) => {
+    console.log("We are listening Port:" + port);
 });
 
