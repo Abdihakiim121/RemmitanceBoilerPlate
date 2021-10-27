9223cd857fdb16d9d4da5eb6456862dd8804f192
Abdihakiim <cabdixakiin121@gmail.com>
2021-10-27 14:13:59 +0300

    Procfile and Auth js Updated

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,29 +1,28 @@
 var status = require('http-status');
-const {ApiError} = require('../payload/apErrors')
+const { ApiError } = require('../payload/apErrors')
 const apiResponse = require('../payload/apiResponse')
 const jwt = require('jsonwebtoken');
 //const authService = require('../service/auth.service')
 
 
-const auth =(req, res, next) =>{
+const auth = (req, res, next) => {
     let authHeaders = req.headers.authorization;
-    let token =authHeaders.split(' ')[1];
-    console.log("accessToken "+token);
-    if(!token){
-       // console.log("hello" +token );
+    let token = authHeaders.split(' ')[1];
+   // console.log("accessToken " + token);
+    if (!token) {
+        // console.log("hello" +token );
         throw new ApiError(401, 'Pleasse provide a token')
     }
-    
+
     let response = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    if(response)
-    {
+    if (response) {
         next();
     }
 
-    throw new ApiError (401, 'Yoour Token has expired, please login again');
+    throw new ApiError(401, 'Yoour Token has expired, please login again');
 
 }
 
-module.exports={
+module.exports = {
     auth
 }
