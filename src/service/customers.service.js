dd82426f2a666dc54d6fa5ea9b2c34bcf6dfc005
Abdihakiim <cabdixakiin121@gmail.com>
2021-11-30 05:51:07 +0300

    Updated

--- src/service/customers.service.js
+++ src/service/customers.service.js
@@ -13,7 +13,14 @@ const getAllCustomers = async () => {
     return await customerModel.getCustomers();
 }
 
+const createCustomers = async (user) =>{
+ 
+    logger.info("Creating user ");
+    return await customerModel.createCustomers(user);
+}
+
 
 module.exports ={
     getAllCustomers,
+    createCustomers
 }
